80c037350a008adf14e2a88bcf7a5f83c12fda58
Mritter26 <Ritter.1442@osu.edu>
2015-08-02 17:27:11 -0400

    Initial progress

---
+++ Recorder/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(308, 34);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Recorder.Properties.Resources.offIcon;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 31);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 34);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Recorder/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recorder
+{
+    public partial class Form1 : Form
+    {
+        public static string CaptureSequence = "";
+        public static bool bCapRun = false, bThreadRunning = false;
+        BackgroundWorker CaptureWatcher = new BackgroundWorker();
+        public static AutoResetEvent endKeyCaught = new AutoResetEvent(false);
+
+        public Form1()
+        {
+            CaptureWatcher.WorkerSupportsCancellation = true;
+            CaptureWatcher.DoWork += CaptureWatcher_DoWork;
+            InitializeComponent();
+            bThreadRunning = true;
+            CaptureWatcher.RunWorkerAsync();
+        }
+
+        void CaptureWatcher_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (bThreadRunning)
+            {
+                endKeyCaught.WaitOne();
+
+                toolStripButton1_Click(null, new EventArgs());
+            }
+        }
+
+        public void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (bCapRun)
+            {
+                bCapRun = false;
+                InterceptMouse.StopCapture();
+                InterceptKeyboard.StopCapture();
+                Console.WriteLine(CaptureSequence);
+                toolStripButton1.Image = global::Recorder.Properties.Resources.offIcon;
+            }
+            else
+            {
+                bCapRun = true;
+                InterceptKeyboard.StartCapture();
+                InterceptMouse.StartCapture();
+                toolStripButton1.Image = global::Recorder.Properties.Resources.recordIcon;
+            }
+        }
+    }
+}
